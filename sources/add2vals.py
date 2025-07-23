66726abf02402e2ebe43d0506d43f2fa063ef115
soniyagupta0813 <soniyagupta0813@gmail.com>
2025-07-22 22:00:09 -0400

    test new code

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -1,25 +1,13 @@
-'''
-A simple command line tool that takes 2 values and adds them together using
-the calc.py library's 'add2' function.
-'''
-
 import sys
-import calc
 
-argnumbers = len(sys.argv) - 1
+def add(a, b):
+    return a + b
 
-if argnumbers == 2 :
-    print("")
-    print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
-    print("")
-    sys.exit(0)
-
-if argnumbers != 2 :
-    print("")
-    print("You entered " + str(argnumbers) + " value/s.")
-    print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
-    print("")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python add2vals.py <num1> <num2>")
+    else:
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+        result = add(x, y)
+        print(f"Sum: {result}")
